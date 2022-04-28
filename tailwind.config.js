99fc8cdf59ddfa63dc069781211f6bf3c13b36f5
tommyxchow <tommyxchow@gmail.com>
2022-04-28 03:24:14 -0400

    Config fonts & prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
