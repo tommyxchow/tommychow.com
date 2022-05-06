129e4e61d9ac851394f43dd92bc55f475c95d317
tommyxchow <tommyxchow@gmail.com>
2022-05-06 17:08:17 -0400

    Update content & styles

--- src/components/header-link.tsx
+++ src/components/header-link.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HeaderLink = (link: HeaderLinkInfo) => {
   return (
     <a
-      className='flex items-center gap-1 opacity-70 transition hover:opacity-100 focus:opacity-100 active:scale-95'
+      className='flex items-center gap-1 font-medium opacity-70 transition hover:opacity-100 focus:opacity-100 active:scale-95'
       href={link.href}
       target='_blank'
       rel='noreferrer'
