4402606fcb259cac61e2879a60137c12a7f036e3
tommyxchow <tommyxchow@gmail.com>
2022-05-09 02:18:54 -0400

    Update fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
           crossOrigin='anonymous'
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap'
+          href='https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Manrope:wght@200;300;400;500;600;700;800&display=swap'
           rel='stylesheet'
         />
       </Head>
