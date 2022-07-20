92246e14a4ea72dfabed9bfc228e33c1c9ba3407
Tommy Chow <tommyxchow@gmail.com>
2022-07-20 13:11:54 -0400

    Tweak navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -24,8 +24,8 @@ const NavBar = ({ links }: NavBarProps) => {
   }
 
   return (
-    <header className='sticky inset-0 z-50 flex flex-col p-4 pb-0 backdrop-blur md:gap-2 lg:static lg:gap-4 lg:p-8 lg:py-32'>
-      <div className='flex items-end justify-between'>
+    <header className='sticky inset-0 z-50 flex flex-col p-4 pb-0 backdrop-blur md:gap-2 lg:static lg:gap-4 lg:p-8 lg:pt-32 lg:pb-16'>
+      <div className='flex justify-between'>
         <h1
           className={`text-4xl font-extrabold md:text-5xl lg:text-6xl ${
             router.pathname == '/'
@@ -39,37 +39,28 @@ const NavBar = ({ links }: NavBarProps) => {
         </h1>
 
         <button
-          className='rounded-md border-2 p-1 sm:hidden'
+          className='text-2xl md:text-3xl lg:text-4xl'
           onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')}
         >
           {theme == 'dark' ? <HiSun /> : <HiMoon />}
         </button>
       </div>
 
-      <div className='flex justify-between'>
-        <nav className='flex gap-2 overflow-auto pb-2 sm:gap-4 sm:overflow-visible sm:pb-0 lg:gap-8'>
-          {routes.map((route) => (
-            <Link key={route} href={'/' + route.toLowerCase()}>
-              <a
-                className={`text-2xl font-bold md:text-3xl lg:text-5xl ${
-                  router.pathname == '/' + route.toLowerCase()
-                    ? undefined
-                    : 'text-neutral-400 dark:text-neutral-500'
-                }`}
-              >
-                {route}
-              </a>
-            </Link>
-          ))}
-        </nav>
-
-        <button
-          className='hidden text-2xl font-bold text-neutral-400 dark:text-neutral-500 sm:block md:text-3xl lg:text-5xl'
-          onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')}
-        >
-          {theme == 'dark' ? 'Light' : 'Dark'}
-        </button>
-      </div>
+      <nav className='flex gap-2 overflow-auto pb-2 lg:gap-8'>
+        {routes.map((route) => (
+          <Link key={route} href={'/' + route.toLowerCase()}>
+            <a
+              className={`text-2xl font-bold md:text-3xl lg:text-5xl ${
+                router.pathname == '/' + route.toLowerCase()
+                  ? undefined
+                  : 'text-neutral-400 dark:text-neutral-500'
+              }`}
+            >
+              {route}
+            </a>
+          </Link>
+        ))}
+      </nav>
     </header>
   );
 };
