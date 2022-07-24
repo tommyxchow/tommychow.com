c70bb35cfd3134a408d5d40fd98be3629fee8d41
Tommy Chow <tommyxchow@gmail.com>
2022-07-24 00:46:47 -0400

    Tweak navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -23,43 +23,45 @@ const NavBar = () => {
   }
 
   return (
-    <header className='sticky inset-0 z-50 flex grid-cols-3 justify-between gap-2 overflow-auto p-4 backdrop-blur sm:grid sm:gap-8 sm:p-8'>
-      <nav>
-        <Link href='/'>
-          <a
-            className={`w-fit whitespace-nowrap border-b border-lime-500 font-semibold transition dark:border-lime-400 ${
-              router.pathname == '/'
-                ? undefined
-                : 'border-opacity-0 text-neutral-400 hover:border-opacity-50 dark:border-opacity-0 dark:text-neutral-500 dark:hover:border-opacity-50'
-            }`}
-          >
-            Tommy Chow
-          </a>
-        </Link>
-      </nav>
-
-      <nav className='col-span-2 flex justify-between gap-2'>
-        {routes.map((route) => (
-          <Link key={route} href={'/' + route.toLowerCase()}>
+    <header className='sticky inset-0 z-50 flex w-full justify-center overflow-auto bg-neutral-100 bg-opacity-20 backdrop-blur transition dark:bg-neutral-900 dark:bg-opacity-20'>
+      <div className='flex w-full max-w-screen-md grid-cols-3 justify-between gap-2 p-4 sm:grid sm:gap-8 sm:p-8'>
+        <nav>
+          <Link href='/'>
             <a
-              className={`border-b border-lime-500 font-semibold transition dark:border-lime-400 ${
-                router.pathname == '/' + route.toLowerCase()
+              className={`w-fit whitespace-nowrap border-b border-lime-500 font-semibold transition dark:border-lime-400 ${
+                router.pathname == '/'
                   ? undefined
                   : 'border-opacity-0 text-neutral-400 hover:border-opacity-50 dark:border-opacity-0 dark:text-neutral-500 dark:hover:border-opacity-50'
               }`}
             >
-              {route}
+              Tommy Chow
             </a>
           </Link>
-        ))}
+        </nav>
 
-        <button
-          className='px-1'
-          onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')}
-        >
-          {theme == 'dark' ? <HiSun /> : <HiMoon />}
-        </button>
-      </nav>
+        <nav className='col-span-2 flex justify-between gap-2'>
+          {routes.map((route) => (
+            <Link key={route} href={'/' + route.toLowerCase()}>
+              <a
+                className={`border-b border-lime-500 font-semibold transition dark:border-lime-400 ${
+                  router.pathname == '/' + route.toLowerCase()
+                    ? undefined
+                    : 'border-opacity-0 text-neutral-400 hover:border-opacity-50 dark:border-opacity-0 dark:text-neutral-500 dark:hover:border-opacity-50'
+                }`}
+              >
+                {route}
+              </a>
+            </Link>
+          ))}
+
+          <button
+            className='px-1'
+            onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')}
+          >
+            {theme == 'dark' ? <HiSun /> : <HiMoon />}
+          </button>
+        </nav>
+      </div>
     </header>
   );
 };
