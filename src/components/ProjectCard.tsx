a30a30f7d84e1cc2014600355e1184198be22e8b
Tommy Chow <tommyxchow@gmail.com>
2022-08-26 04:16:08 -0400

    Update project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,6 +6,7 @@ const ProjectCard = ({
   id,
   name,
   thumbnailLink,
+  shortDescription,
   dateCompleted,
 }: ProjectInfo) => {
   let formattedDate = 'Ongoing';
@@ -19,15 +20,17 @@ const ProjectCard = ({
 
   return (
     <div
-      className='group relative aspect-video overflow-clip shadow-lg transition hover:scale-105 hover:shadow-xl active:scale-95 active:shadow-none'
+      className='relative aspect-video overflow-clip shadow-lg transition duration-300 hover:scale-105 hover:shadow-xl active:scale-95 active:shadow-none'
       id={id}
     >
       <Link href={'/projects/' + id}>
-        <a className='absolute z-10 flex h-full w-full flex-col items-center justify-center p-8 opacity-0 transition hover:bg-black hover:bg-opacity-80 hover:opacity-100'>
+        <a className='absolute z-10 flex h-full w-full flex-col justify-end bg-gradient-to-b from-transparent to-black p-4 transition'>
           <h2 className='text-xl font-semibold text-neutral-200 sm:text-2xl'>
             {name}
           </h2>
-          <p className='text-neutral-300'>{formattedDate}</p>
+          <p className='text-sm text-neutral-300 sm:text-base'>
+            {shortDescription}
+          </p>
         </a>
       </Link>
 
