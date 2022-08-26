83607d53c5c4948e67e1393fd7b781fa9bb25a9f
Tommy Chow <tommyxchow@gmail.com>
2022-08-26 05:38:43 -0400

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+const Custom404 = () => {
+  return (
+    <article className='flex min-h-screen flex-col items-center justify-center gap-4'>
+      <h1 className='font-mono text-2xl'>404</h1>
+      <p className='text-neutral-500 dark:text-neutral-400'>
+        Looks like this page doesn&apos;t exist...
+      </p>
+    </article>
+  );
+};
+
+export default Custom404;
