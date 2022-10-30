9a7353423f647cee6adf098642eed228ff1689d3
Tommy Chow <tommyxchow@gmail.com>
2022-10-30 06:13:39 -0400

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@fontsource/inter/500.css';
 import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
 import '@fontsource/jetbrains-mono';
+import { Analytics } from '@vercel/analytics/react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
@@ -11,31 +12,35 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <ThemeProvider attribute='class' disableTransitionOnChange>
-      <div className='m-auto flex w-full max-w-screen-sm flex-col'>
-        <Header />
+    <>
+      <ThemeProvider attribute='class' disableTransitionOnChange>
+        <div className='m-auto flex w-full max-w-screen-sm flex-col'>
+          <Header />
 
-        <div className='flex min-h-screen grid-cols-4 flex-col sm:grid sm:gap-8'>
-          <NavBar />
+          <div className='flex min-h-screen grid-cols-4 flex-col sm:grid sm:gap-8'>
+            <NavBar />
 
-          <div className='col-span-full col-start-2'>
-            <AnimatePresence mode='wait'>
-              <motion.div
-                key={router.route}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5 }}
-                exit={{ opacity: 0 }}
-              >
-                <Component {...pageProps} />
-              </motion.div>
-            </AnimatePresence>
+            <div className='col-span-full col-start-2'>
+              <AnimatePresence mode='wait'>
+                <motion.div
+                  key={router.route}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+                  exit={{ opacity: 0 }}
+                >
+                  <Component {...pageProps} />
+                </motion.div>
+              </AnimatePresence>
+            </div>
           </div>
-        </div>
 
-        <Footer />
-      </div>
-    </ThemeProvider>
+          <Footer />
+        </div>
+      </ThemeProvider>
+
+      <Analytics />
+    </>
   );
 }
 
