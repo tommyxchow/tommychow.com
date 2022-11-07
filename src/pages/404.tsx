79bc5d7dfdf655f8124535e178f7ee9404037cb4
Tommy Chow <tommyxchow@gmail.com>
2022-11-07 05:18:16 -0500

    Update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,15 @@
+import Layout from '../components/Layout';
+
 const Custom404 = () => {
   return (
-    <article className='flex min-h-screen flex-col items-center justify-center gap-4'>
-      <h1 className='font-mono text-2xl'>404</h1>
-      <p className='text-neutral-600 dark:text-neutral-400'>
-        Looks like this page doesn&apos;t exist...
-      </p>
-    </article>
+    <Layout title='404' description='Page not found.'>
+      <article className='flex flex-col items-center justify-center gap-4'>
+        <h1 className='font-mono text-2xl'>404</h1>
+        <p className='text-neutral-600 dark:text-neutral-400'>
+          Looks like this page doesn&apos;t exist...
+        </p>
+      </article>
+    </Layout>
   );
 };
 
