a0978c0c6ad4f383ef0a13c895535e36f96b1357
Tommy Chow <tommyxchow@gmail.com>
2022-11-07 05:19:58 -0500

    Fix titles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 
 const Custom404 = () => {
   return (
-    <Layout title='404' description='Page not found.'>
+    <Layout title='404 | Tommy Chow' description='Page not found.'>
       <article className='flex flex-col items-center justify-center gap-4'>
         <h1 className='font-mono text-2xl'>404</h1>
         <p className='text-neutral-600 dark:text-neutral-400'>
