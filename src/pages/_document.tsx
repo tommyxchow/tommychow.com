7eddcb375baa4ca6b49101e1e43065b54bc48f15
Tommy Chow <tommyxchow@gmail.com>
2023-02-02 23:16:46 -0500

    Fix smooth scrolling behavior

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang='en' className='scroll-smooth'>
+    <Html lang='en'>
       <Head>
         <link
           rel='icon'
