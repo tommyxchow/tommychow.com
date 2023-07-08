23dd0713dbacb25e3c9a65fe0464adee3a0bb7a2
Tommy Chow <tommyxchow@gmail.com>
2023-07-08 19:03:06 -0400

    Remove email link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,16 +34,16 @@ const Home = () => {
         </div>
 
         <ul className='flex flex-wrap gap-2'>
-          {links.slice(0, 4).map((link) => (
+          {links.slice(0, 3).map((link) => (
             <li key={link.title}>
               <a
-                className='flex w-fit items-center gap-2 rounded-full bg-neutral-200 px-4 py-2 text-sm font-medium shadow transition hover:opacity-60 active:scale-95 active:shadow-none dark:bg-neutral-900'
+                className='flex w-fit items-center gap-2 rounded-full bg-neutral-200 px-4 py-2 font-medium shadow transition hover:opacity-60 active:scale-95 active:shadow-none dark:bg-neutral-900'
                 href={link.href}
                 target='_blank'
                 rel='noreferrer'
               >
                 {link.icon}
-                {link.title}
+                <span className='text-sm'>{link.title}</span>
               </a>
             </li>
           ))}
