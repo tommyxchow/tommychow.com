38adaa24e446eb09e3565a681dd3c348f5ba5433
Tommy Chow <tommyxchow@gmail.com>
2023-08-03 00:31:16 -0400

    Add ogimage

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,31 @@ export default function Document() {
           href='/assets/images/favicon.svg'
         />
         <link rel='icon' type='image/png' href='/assets/images/favicon.png' />
+
+        <meta property='og:url' content='https://www.tommychow.com/' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Tommy Chow' />
+        <meta
+          property='og:description'
+          content='Software engineer and fried food lover.'
+        />
+        <meta
+          property='og:image'
+          content='https://www.tommychow.com/ogimage.png'
+        />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content='tommychow.com' />
+        <meta property='twitter:url' content='https://www.tommychow.com/' />
+        <meta name='twitter:title' content='Tommy Chow' />
+        <meta
+          name='twitter:description'
+          content='Software engineer and fried food lover.'
+        />
+        <meta
+          name='twitter:image'
+          content='https://www.tommychow.com/ogimage.png'
+        />
       </Head>
       <body className='bg-neutral-100 text-neutral-900 selection:bg-sky-600 selection:text-neutral-100 dark:bg-neutral-950 dark:text-neutral-100 dark:selection:bg-sky-400 dark:selection:text-neutral-900'>
         <Main />
