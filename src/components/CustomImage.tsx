c10f8ce6c00500ef7fb4649c1fd97c3962267e3a
Tommy Chow <tommyxchow@gmail.com>
2023-12-13 19:09:43 -0500

    Use blur image placeholder

--- src/components/CustomImage.tsx
+++ src/components/CustomImage.tsx
@@ -1,24 +1,15 @@
 'use client';
 
 import Image, { type ImageProps } from 'next/image';
-import { useState } from 'react';
-import { twMerge } from 'tailwind-merge';
 
-export function CustomImage({ className, priority, alt, src }: ImageProps) {
-  const [isLoading, setIsLoading] = useState(true);
-
+export function CustomImage({ priority, alt, src }: ImageProps) {
   return (
     <Image
-      className={twMerge(
-        'shadow-xl transition duration-500 ease-out',
-        isLoading ? 'opacity-0' : 'opacity-100',
-        className,
-      )}
       fill={typeof src === 'string' && true}
       priority={priority}
-      onLoad={() => setIsLoading(false)}
       alt={alt}
       src={src}
+      placeholder='blur'
     />
   );
 }
