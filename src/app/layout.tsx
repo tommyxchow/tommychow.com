24872f43fd15abee6ff035398751bb307f05354e
Tommy Chow <tommyxchow@gmail.com>
2023-12-14 00:33:19 -0500

    Increase global underline offset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <Providers>
         <body
           className={twJoin(
-            'mx-auto flex min-h-screen max-w-screen-sm flex-col bg-neutral-50 px-4 font-sans text-neutral-900 dark:bg-neutral-950 dark:text-neutral-100 lg:px-0',
+            'mx-auto flex min-h-screen max-w-screen-sm flex-col bg-neutral-50 px-4 font-sans text-neutral-900 underline-offset-4 dark:bg-neutral-950 dark:text-neutral-100 lg:px-0',
             fontSans.variable,
             fontMono.variable,
           )}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,7 @@ import { source } from '@/lib/links';
 export function Footer() {
   return (
     <footer className='flex py-8 text-sm text-neutral-600 dark:text-neutral-400 md:px-0'>
-      <a
-        className='underline-offset-2 hover:underline'
-        href={source}
-        target='_blank'
-      >
+      <a className='hover:underline' href={source} target='_blank'>
         Last updated on Dec. 2023
       </a>
     </footer>
