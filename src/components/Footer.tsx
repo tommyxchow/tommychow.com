ae002e75cbb03835bea3d43ae0e8478b4dbf197b
Tommy Chow <tommyxchow@gmail.com>
2023-12-14 00:21:31 -0500

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,13 @@ import { source } from '@/lib/links';
 
 export function Footer() {
   return (
-    <footer className='flex justify-end py-8 text-sm text-neutral-600 dark:text-neutral-400 md:px-0'>
+    <footer className='flex py-8 text-sm text-neutral-600 dark:text-neutral-400 md:px-0'>
       <a
         className='underline-offset-2 hover:underline'
         href={source}
         target='_blank'
       >
-        Last updated Oct. 2023
+        Last updated on Dec. 2023
       </a>
     </footer>
   );
