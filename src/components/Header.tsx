dacddd772a66bc4b919500ebd85fac9a3c00015f
Tommy Chow <tommyxchow@gmail.com>
2023-12-15 05:25:03 -0500

    Fix theme button alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,23 +17,25 @@ export function Header() {
           <h1>Tommy Chow</h1>
         </Link>
 
-        <ul className='flex gap-4'>
-          {routes.map((route) => (
-            <li key={route}>
-              <Link
-                className={twJoin(
-                  path?.includes(route.toLowerCase()) && 'underline',
-                )}
-                href={'/' + route.toLowerCase()}
-              >
-                {route}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+        <div className='flex items-center gap-4 self-start'>
+          <ul className='flex gap-4'>
+            {routes.map((route) => (
+              <li key={route}>
+                <Link
+                  className={twJoin(
+                    path?.includes(route.toLowerCase()) && 'underline',
+                  )}
+                  href={'/' + route.toLowerCase()}
+                >
+                  {route}
+                </Link>
+              </li>
+            ))}
+          </ul>
 
-      <ThemeToggle />
+          <ThemeToggle />
+        </div>
+      </nav>
     </header>
   );
 }
