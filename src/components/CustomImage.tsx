f1fd7c34125ab368721d7b3212536889e53dc7b6
Tommy Chow <tommyxchow@gmail.com>
2023-12-15 05:33:00 -0500

    Add shadow to images

--- src/components/CustomImage.tsx
+++ src/components/CustomImage.tsx
@@ -5,6 +5,7 @@ import Image, { type ImageProps } from 'next/image';
 export function CustomImage({ priority, alt, src }: ImageProps) {
   return (
     <Image
+      className='shadow-lg'
       fill={typeof src === 'string' && true}
       priority={priority}
       alt={alt}
