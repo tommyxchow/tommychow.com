1a17057ac668e8a2774662f45fae560ca4fd40d1
Tommy Chow <tommyxchow@gmail.com>
2023-12-15 05:42:54 -0500

    Tweak prose colors

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -7,7 +7,7 @@ export function Prose({
   return (
     <div
       className={twMerge(
-        'prose prose-neutral max-w-none dark:prose-invert',
+        'prose prose-neutral max-w-none dark:prose-invert prose-headings:text-neutral-900 prose-a:text-neutral-900 dark:prose-headings:text-neutral-100 dark:prose-a:text-neutral-100',
         className,
       )}
     >
