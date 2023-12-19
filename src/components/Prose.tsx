435f2bad98d46f31ff69f04370d001b859730f60
Tommy Chow <tommyxchow@gmail.com>
2023-12-19 03:16:28 -0500

    Tweak prose heading margins

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -7,7 +7,7 @@ export function Prose({
   return (
     <div
       className={twMerge(
-        'prose prose-zinc max-w-none dark:prose-invert prose-headings:text-zinc-900 prose-a:text-zinc-900 dark:prose-headings:text-zinc-100 dark:prose-a:text-zinc-100',
+        'prose prose-zinc max-w-none dark:prose-invert prose-headings:text-zinc-900 prose-h2:mt-4 prose-a:text-zinc-900 dark:prose-headings:text-zinc-100 dark:prose-a:text-zinc-100 sm:prose-h2:mt-8',
         className,
       )}
     >
