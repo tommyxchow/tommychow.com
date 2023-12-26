9b02d5dc86cb7244afcb14b185179376964757a6
Tommy Chow <tommyxchow@gmail.com>
2023-12-26 04:58:31 -0500

    Remove unused className prop from CustomImage

--- src/components/CustomImage.tsx
+++ src/components/CustomImage.tsx
@@ -1,24 +1,13 @@
 import Image, { type ImageProps } from 'next/image';
-import { twMerge } from 'tailwind-merge';
 
 interface CustomImageProps extends ImageProps {
   caption?: string;
 }
 
-export function CustomImage({
-  className,
-  alt,
-  caption,
-  ...rest
-}: CustomImageProps) {
+export function CustomImage({ alt, caption, ...rest }: CustomImageProps) {
   return (
     <figure>
-      <Image
-        className={twMerge('shadow-lg', className)}
-        alt={alt}
-        placeholder='blur'
-        {...rest}
-      />
+      <Image className='shadow-lg' alt={alt} placeholder='blur' {...rest} />
 
       {caption && <figcaption>{caption}</figcaption>}
     </figure>
