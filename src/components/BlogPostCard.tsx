e06ba065c3e167062a776ce23228031313e0b6ac
Tommy Chow <tommyxchow@gmail.com>
2024-05-12 16:02:56 -0700

    Tweak styling

--- src/components/BlogPostCard.tsx
+++ src/components/BlogPostCard.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function BlogPostCard({ id, title, date }: BlogPost) {
   return (
     <Link href={`/blog/${id}`}>
-      <article className='-mx-4 flex flex-col gap-2 rounded-lg p-4 transition-[background] hover:bg-zinc-100 hover:dark:bg-zinc-900'>
+      <article className='-mx-4 flex flex-col gap-2 p-4 transition-[background] hover:bg-zinc-100 hover:dark:bg-zinc-900 sm:rounded-lg'>
         <h3 className='text-lg font-semibold'>{title}</h3>
         <time
           className='text-zinc-500 dark:text-zinc-400'
