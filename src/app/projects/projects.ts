c28171f5d5732d82e918333d203dc6e3dfa82282
Tommy Chow <tommyxchow@gmail.com>
2024-05-12 19:35:36 -0700

    Update project data

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -6,59 +6,41 @@ import vewdditThumbnail from './images/vewddit.webp';
 
 export interface ProjectInfo {
   name: string;
-  thumbnail: StaticImageData;
   description: string;
-  dateCompleted?: string;
+  thumbnail: StaticImageData;
   url: string;
   githubUrl: string;
-  technologies: string[];
 }
 
 export const projects = [
   {
     name: 'Frosty',
     description:
-      'Watch and chat on Twitch with your favorite third-party emotes on mobile.',
+      'Watch Twitch streams on iOS and Android with third-party emote support.',
     thumbnail: frostyThumbnail,
-    dateCompleted: '2023-01-15',
     url: 'https://frostyapp.io/',
     githubUrl: 'https://github.com/tommyxchow/frosty',
-    technologies: [
-      'Dart',
-      'Fastlane',
-      'Figma',
-      'Firebase',
-      'Flutter',
-      'GitHub Actions',
-      'MobX',
-    ],
   },
   {
     name: 'Hatchet',
-    description: 'A fast, modern, and simple web client for Hacker News.',
+    description: 'A more modern way to browse and read Hacker News.',
     thumbnail: hatchetThumbnail,
-    dateCompleted: '2023-12-01',
     url: 'https://hatchetnews.vercel.app/',
     githubUrl: 'https://github.com/tommyxchow/hatchet',
-    technologies: ['React', 'Next.js', 'Tailwind CSS', 'TypeScript'],
   },
   {
     name: 'Vewddit',
-    description: 'A web app for Reddit focused on browsing visual media.',
+    description: 'Explore media on Reddit with AI comment summaries.',
     thumbnail: vewdditThumbnail,
-    dateCompleted: '2023-11-01',
     url: 'https://vewddit.vercel.app/',
     githubUrl: 'https://github.com/tommyxchow/vewddit',
-    technologies: ['React', 'Next.js', 'Tailwind CSS', 'TypeScript'],
   },
   {
     name: 'Stawks',
     description:
       'View price history charts, financial details, and relevant news for stocks.',
     thumbnail: stawksThumbnail,
-    dateCompleted: '2022-11-15',
     url: 'https://stawks.vercel.app/',
     githubUrl: 'https://github.com/tommyxchow/stawks',
-    technologies: ['React', 'Next.js', 'Tailwind CSS', 'TypeScript', 'Vercel'],
   },
 ] satisfies ProjectInfo[];
