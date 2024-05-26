4f77ad2dc03d74c844def707954fca31ab45590d
Tommy Chow <tommyxchow@gmail.com>
2024-05-25 20:37:57 -0700

    Fix card hover on light mode

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,7 +9,7 @@ export default function ProjectCard({
 }: ProjectInfo) {
   return (
     <a aria-labelledby={name} href={url} target='_blank'>
-      <article className='-mx-4 flex flex-col gap-4 p-4 transition-[background] hover:bg-zinc-100 hover:dark:bg-zinc-900 sm:flex-row sm:items-center sm:rounded-lg'>
+      <article className='-mx-4 flex flex-col gap-4 p-4 transition-[background] hover:bg-zinc-200 dark:hover:bg-zinc-900 sm:flex-row sm:items-center sm:rounded-lg'>
         <div className='sm:w-1/3 sm:shrink-0'>
           <CustomImage
             priority
