ec33e86ceb5e12bd30a8ebe7a9ffa6c6df5f69d8
Tommy Chow <tommyxchow@gmail.com>
2025-01-01 22:08:05 -0500

    add initial metadata

--- src/app/gallery/[id]/page.tsx
+++ src/app/gallery/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { CustomImage } from '@/components/CustomImage';
 import { getSortedImagesByDate } from '@/lib/server-utils';
+import { formatDate } from '@/lib/utils';
 import Link from 'next/link';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
@@ -19,25 +20,28 @@ export default async function GalleryPage({
 
   const allImages = await getSortedImagesByDate();
 
-  const selectedImage =
-    allImages.find(({ file }) => file === id)?.file ?? allImages[0].file;
+  const { file, exifData } =
+    allImages.find(({ file }) => file === id) ?? allImages[0];
 
   return (
     <section className='flex flex-col gap-4'>
-      <figure className='sticky top-20 z-10 -mx-4 flex flex-col items-center gap-4 bg-zinc-100 p-4 dark:bg-zinc-950'>
+      <figure className='sticky top-20 z-10 -mx-4 flex flex-col gap-4 bg-zinc-100 p-4 dark:bg-zinc-950'>
         <div className='relative aspect-square size-full'>
           <Zoom classDialog='custom-zoom'>
             <CustomImage
               className='object-contain shadow-none'
-              src={`/gallery/images/${selectedImage}`}
-              alt={`Gallery image ${selectedImage}`}
+              src={`/gallery/images/${file}`}
+              alt={`Gallery image ${file}`}
               sizes='100vw'
               fill
             />
           </Zoom>
         </div>
 
-        <figcaption className='font-mono text-sm'>{selectedImage}</figcaption>
+        <figcaption className='flex flex-col items-center font-mono text-sm text-zinc-500 dark:text-zinc-400'>
+          <p>{formatDate(exifData.DateTimeOriginal, true, true)}</p>
+          <p>{exifData.Model}</p>
+        </figcaption>
       </figure>
 
       <ul className='grid grid-cols-3 gap-4'>
