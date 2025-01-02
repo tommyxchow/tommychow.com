877a261dbd47c55bb9d87b8697532a3d7a035ca8
Tommy Chow <tommyxchow@gmail.com>
2025-01-02 02:16:48 -0500

    format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { LastUpdated } from './LastUpdated';
 
 export function Footer() {
   return (
-    <footer className='mt-16 flex flex-col justify-between gap-2 text-sm text-zinc-500 dark:text-zinc-400 sm:flex-row'>
+    <footer className='mt-16 flex flex-col justify-between gap-2 text-sm text-zinc-500 sm:flex-row dark:text-zinc-400'>
       <ul className='flex gap-4'>
         {links.map((link) => (
           <li key={link.title}>
