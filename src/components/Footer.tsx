b46ad7f650a6a9c00109d9f1e96c0500671dce1f
Tommy Chow <tommyxchow@gmail.com>
2025-01-03 03:08:43 -0500

    only show footer on home page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,17 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { LastUpdated } from './LastUpdated';
 
 export function Footer() {
+  const path = usePathname();
+
+  if (path !== '/') {
+    return null;
+  }
+
   return (
-    <footer className='mt-16 flex items-center justify-center gap-2 text-sm text-zinc-500 sm:flex-row dark:text-zinc-400'>
+    <footer className='mt-8 flex justify-center text-sm text-zinc-400 dark:text-zinc-500'>
       <LastUpdated />
     </footer>
   );
