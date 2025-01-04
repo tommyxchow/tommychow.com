67452952bd8dd1f22df478520900284ad49f24bf
Tommy Chow <tommyxchow@gmail.com>
2025-01-04 05:05:13 -0500

    fix animation when zooming

--- src/app/gallery/[id]/page.tsx
+++ src/app/gallery/[id]/page.tsx
@@ -25,9 +25,9 @@ export default async function GalleryPage({
   return (
     <section className='flex flex-col gap-4'>
       <figure className='sticky top-20 z-10 -mx-4 flex flex-col gap-4 border-b border-zinc-300 bg-zinc-100 px-4 pb-4 dark:border-zinc-800 dark:bg-zinc-950'>
-        <div className='relative aspect-[4/3]'>
+        <div className='relative aspect-[4/3] duration-300 animate-in fade-in'>
           <CustomImage
-            className='mx-auto mt-auto !size-auto max-h-full max-w-full duration-300 animate-in fade-in'
+            className='mx-auto mt-auto !size-auto max-h-full max-w-full'
             src={`/gallery/images/${file}`}
             alt={`Gallery image ${file}`}
             sizes='100vw'
