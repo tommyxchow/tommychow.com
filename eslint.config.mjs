fe1e36d9fc62ede51c91a726a697f62835da4d91
Tommy Chow <tommyxchow@gmail.com>
2025-07-06 22:29:06 -0700

    migrate to eslint v9 and revise rules

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+// @ts-check
+import eslintReact from '@eslint-react/eslint-plugin'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintJs from '@eslint/js'
+import prettier from 'eslint-config-prettier/flat'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import tseslint from 'typescript-eslint'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+export default tseslint.config(
+  eslintJs.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintReact.configs['recommended-type-checked'],
+  ...compat.extends('next/core-web-vitals', 'plugin:jsx-a11y/recommended'),
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      eqeqeq: ['error', 'smart'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { fixStyle: 'inline-type-imports' },
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/switch-exhaustiveness-check': 'error',
+      'jsx-a11y/control-has-associated-label': 'warn',
+    },
+  },
+  prettier,
+)
