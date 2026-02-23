083a44eaa3fa4ae9d1d657e8daadf983b129c7e4
Tommy Chow <tommyxchow@gmail.com>
2026-02-22 20:35:39 -0800

    remove loading

--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className='text-muted-foreground flex flex-1 items-center justify-center py-16 text-sm'>
-      Loading...
-    </div>
-  )
-}
